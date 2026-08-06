f66e2f8a70b380e4b6a2507fde0e0d1316fea6bd
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2026-08-07 04:02:59 +0500

    connent database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from src.utils.db import engine, Base 
 
+Base.metadata.create_all(engine)
 
 app = FastAPI(title="this is my task managment application")
 
---
+++ src/utils/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker , declarative_base
+from src.utils.settings import settings
+Base = declarative_base()
+engine = create_engine(settings.DB_URI)
+
+local_session = sessionmaker(bind=engine)
+
+
+def get_db():
+    session = local_session()
+    try: 
+        yield session
+    finally: 
+        session.close()
