3de0432058a7907fede35516c689269bbe62b354
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2026-08-11 00:11:08 +0500

    update ..

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -9,7 +9,7 @@ from src.users.models import UserModel
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 @router.post("/" , response_model=TaskResponseSchema , status_code=status.HTTP_201_CREATED)
-def create_task(body : TaskSchema , db : Session = Depends(get_db) ):
+def create_task(body : TaskSchema , db : Session = Depends(get_db) , user : UserModel =  Depends(is_authenticated)):
     return controller.create_task(body , db )
 
 
@@ -20,11 +20,11 @@ def get_all_task(db : Session = Depends(get_db) , user : UserModel =  Depends(is
 
 
 @router.get("/{task_id}", response_model=TaskResponseSchema , status_code=status.HTTP_200_OK)
-def get_single_task(task_id : int , db : Session = Depends(get_db)):
+def get_single_task(task_id : int , db : Session = Depends(get_db), user : UserModel =  Depends(is_authenticated)):
     return controller.get_single_task(task_id, db)
 @router.put("/{task_id}", response_model=TaskResponseSchema , status_code=status.HTTP_201_CREATED)
-def update_task(body: TaskSchema , task_id : int, db : Session = Depends(get_db)):
+def update_task(body: TaskSchema , task_id : int, db : Session = Depends(get_db) , user : UserModel =  Depends(is_authenticated)):
     return controller.update_task(body , task_id , db )
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_task( task_id: int , db : Session = Depends(get_db)):
+def delete_task( task_id: int , db : Session = Depends(get_db) , user : UserModel =  Depends(is_authenticated)):
     return controller.delete_task(task_id , db )
